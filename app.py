bfb68496d4d692093505925071d2fc0711b5509c
Walter Ortiz <walterortiz07@gmail.com>
2025-11-17 22:37:57 -0300

    escuchamos en todas las interfaces

--- app.py
+++ app.py
@@ -191,4 +191,5 @@ def generar_qr(data: str):
 #  MAIN
 # ==========================
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
